45f472238bb10d2e2cf55ab95801ea177724e86b
filatovevgeny13 <76839320+filatovevgeny13@users.noreply.github.com>
2021-04-19 18:45:17 +0500

    Update User.java

--- src/main/java/web/model/User.java
+++ src/main/java/web/model/User.java
@@ -120,7 +120,7 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return roles;
     }
 
     @Override
